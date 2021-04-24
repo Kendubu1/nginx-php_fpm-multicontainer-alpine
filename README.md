c561edaf1be89e8eccaf1a22fc0aa250748bcd45
Kendrick Dubuisson <kedubuis@microsoft.com>
2021-04-23 20:10:18 -0500

    first commit

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 ## Multi-container sample using Nginx & Php-fpm to serve files from Azure mounted storage | Alpine Base - SSH Enabled
 
+"# nginx-php_fpm-multicontainer-alpine" 
--- nginx/init.sh
+++ nginx/init.sh
@@ -2,12 +2,9 @@
 
 echo "Setup openrc ..." && openrc && touch /run/openrc/softlevel
 
-# BEGIN: Initialize ssh daemon as early as possible
 echo Starting ssh service...
 rc-service sshd start
 
-# We want all ssh sesions to start in the /home directory
-echo "cd /home" >> /etc/profile
-
 # Start nginx
-service nginx start
+echo Starting nginx...
+nginx -g "daemon off;"
